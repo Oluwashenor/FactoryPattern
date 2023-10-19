9dd9eb5c0fa0e77f7c7ca81398709ca931ccea7e
Adesina <adeshiname@gmail.com>
2023-10-19 08:21:50 +0100

    Add project files.

---
+++ FactoryPattern/Models/Time.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryPattern.Models;
+
+public class Time : ITime
+{
+    public string CurrentTime { get; set; } = DateTime.Now.ToString();
+}
+
+public interface ITime
+{
+	string CurrentTime { get; set; }
+}
